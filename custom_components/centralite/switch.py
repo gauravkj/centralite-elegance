63da11b963a4f361a1b894ee47af0f928e34ed44
gauravkj <gauravkj@gmail.com>
2026-03-29 13:34:13 -0400

    Update switch.py

--- custom_components/centralite/switch.py
+++ custom_components/centralite/switch.py
@@ -1,22 +1,13 @@
 from __future__ import annotations
 
-import logging
-
 from homeassistant.components.switch import SwitchEntity
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
-from .const import CONF_EXCLUDE_NAMES, CONF_INCLUDE_SWITCHES, DOMAIN
-
-_LOGGER = logging.getLogger(__name__)
+from .const import CONF_INCLUDE_SWITCHES, DOMAIN
 
 ATTR_NUMBER = "number"
-
-
-def _is_ignored(name: str, excluded_prefixes: list[str]) -> bool:
-    """Return True if entity name should be ignored."""
-    return any(name.startswith(prefix) for prefix in excluded_prefixes)
 
 
 async def async_setup_entry(
@@ -25,19 +16,16 @@ async def async_setup_entry(
     async_add_entities: AddEntitiesCallback,
 ) -> None:
     """Set up Centralite switch entities from a config entry."""
-    if not entry.data.get(CONF_INCLUDE_SWITCHES, False):
+    if not entry.options.get(
+        CONF_INCLUDE_SWITCHES,
+        entry.data.get(CONF_INCLUDE_SWITCHES, False),
+    ):
         return
 
     data = hass.data[DOMAIN][entry.entry_id]
     controller = data.controller
-    excluded_prefixes = entry.data.get(CONF_EXCLUDE_NAMES, [])
 
-    entities = []
-    for device in controller.button_switches():
-        name = controller.get_switch_name(device)
-        if not _is_ignored(name, excluded_prefixes):
-            entities.append(CentraliteSwitch(device, controller))
-
+    entities = [CentraliteSwitch(device, controller) for device in controller.button_switches()]
     async_add_entities(entities, True)
 
 
@@ -57,12 +45,12 @@ class CentraliteSwitch(SwitchEntity):
         controller.on_switch_pressed(sw_device, self._on_switch_pressed)
         controller.on_switch_released(sw_device, self._on_switch_released)
 
-    def _on_switch_pressed(self, *args):
+    def _on_switch_pressed(self, *args) -> None:
         """Handle switch press event."""
         self._state = True
         self.schedule_update_ha_state()
 
-    def _on_switch_released(self, *args):
+    def _on_switch_released(self, *args) -> None:
         """Handle switch release event."""
         self._state = False
         self.schedule_update_ha_state()
@@ -79,7 +67,7 @@ class CentraliteSwitch(SwitchEntity):
 
     @property
     def extra_state_attributes(self):
-        """Return device specific attributes."""
+        """Return switch specific attributes."""
         return {ATTR_NUMBER: self._index}
 
     def turn_on(self, **kwargs):
