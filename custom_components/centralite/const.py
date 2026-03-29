c221b2358689f852e1b30228eb43bdb351a0f2ca
gauravkj <gauravkj@gmail.com>
2026-03-29 01:14:47 -0400

    Update const.py

--- custom_components/centralite/const.py
+++ custom_components/centralite/const.py
@@ -1,1 +1,8 @@
+DOMAIN = "centralite"
 
+CONF_PORT = "port"
+CONF_INCLUDE_SWITCHES = "include_switches"
+CONF_INCLUDE_SCENES = "include_scenes"
+CONF_EXCLUDE_NAMES = "exclude_names"
+
+PLATFORMS = ["light", "fan", "switch", "scene"]
